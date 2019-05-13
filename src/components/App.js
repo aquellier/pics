f596e46ecb0caf3275ac8c470923f23556c62d36
antoinequellier <antoinecquellier@gmail.com>
2019-05-13 08:52:42 +0100

    this.setState is not a function typical error

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import axios from 'axios';
 // A promise is an object that gives us a notification when
 // some amount of work is completed, like a network request
 class App extends React.Component {
+  state = { images: [] };
+
   async onSearchSubmit(term) {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
@@ -14,13 +16,14 @@ class App extends React.Component {
       }
     });
 
-    console.log(response.data.results);
+    this.setState({ images: response.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{marginTop: '10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
 
     )
