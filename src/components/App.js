1fded9b5e6047721da09c45d0696a55f51df868c
antoinequellier <antoinecquellier@gmail.com>
2019-05-13 08:30:08 +0100

    requests to unsplash API

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,11 @@ import axios from 'axios';
 class App extends React.Component {
   onSearchSubmit(term) {
     axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID ee45f3b0b4ff95301843eb2d3a0226ecab2ec5771b7cd1fe0dc921a82c5b9ce0',
 
+      }
     });
   }
 
