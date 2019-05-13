2a4cec77750d7a4a1090c352506d1b483dea3a85
antoinequellier <antoinecquellier@gmail.com>
2019-05-13 08:42:16 +0100

    get pictures with async await syntax

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import axios from 'axios';
 
+// A promise is an object that gives us a notification when
+// some amount of work is completed, like a network request
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
         Authorization: 'Client-ID ee45f3b0b4ff95301843eb2d3a0226ecab2ec5771b7cd1fe0dc921a82c5b9ce0',
 
       }
     });
+
+    console.log(response.data.results);
   }
 
   render() {
