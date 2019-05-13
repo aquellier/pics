0031e35d907ecedfa0a7488b6f76afc2d470be35
antoinequellier <antoinecquellier@gmail.com>
2019-05-13 09:18:51 +0100

    display list of images

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import unsplash from '../api/unsplash';
-
+import ImageList from './ImageList';
 // A promise is an object that gives us a notification when
 // some amount of work is completed, like a network request
 class App extends React.Component {
@@ -19,7 +19,7 @@ class App extends React.Component {
     return (
       <div className="ui container" style={{marginTop: '10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        Found: {this.state.images.length} images
+        <ImageList images={this.state.images}/>
       </div>
 
     )
