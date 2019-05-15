6245c0ce3f2f682071aa63eec6799ed928ad2a1a
antoinequellier <antoinecquellier@gmail.com>
2019-05-15 09:53:27 +0100

    callback on imageload

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    console.log(this.imageRef.current.clientHeight);
+  }
+
   render() {
-    const { description, urls } = this.props.images;
+    const { description, urls } = this.props.image;
     return (
       <div>
         <img
+        ref={this.imageRef}
           src={urls.regular}
           alt={description}
         />
